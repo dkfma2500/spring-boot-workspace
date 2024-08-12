c667be9ee6b3af098c1ec4a97920ebb3fa3bf71b
dkfma2500 <dkfma2500@naver.com>
2024-08-12 20:40:17 +0900

    SPRINGBOOT수업

---
+++ ex24_transaction_propagation/src/main/java/com/example/ex24_transaction_propagation/service/LogWriteServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.ex24_transaction_propagation.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ex24_transaction_propagation.dao.Transaction3DAO;
+
+@Service
+public class LogWriteServiceImpl implements LogWriteService {
+    
+@Autowired
+    Transaction3DAO transaction3DAO;
+    @Override
+    public int write(String consumerId, int amount) {
+        try {
+            transaction3DAO.pay(consumerId, amount);
+        return 1;
+        } catch(Exception e) {
+            return 0;
+        }
+    }
+}

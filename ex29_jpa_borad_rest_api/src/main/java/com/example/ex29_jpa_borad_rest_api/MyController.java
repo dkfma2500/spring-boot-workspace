08feccf903ee1bec60ac1c6f9a19038fda1fc112
dkfma2500 <dkfma2500@naver.com>
2024-08-19 21:49:36 +0900

    SPRINGBOOT수업

--- ex29_jpa_borad_rest_api/src/main/java/com/example/ex29_jpa_borad_rest_api/MyController.java
+++ ex29_jpa_borad_rest_api/src/main/java/com/example/ex29_jpa_borad_rest_api/MyController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,19 +45,29 @@ public class MyController {
         }
     }
 
+    // @PostMapping("/write")
+    // public ResponseEntity<Board> write(@RequestParam("writer") String writer,
+    //         @RequestParam("title") String title,
+    //         @RequestParam("content") String content) {
+    //     boardService.write(writer, title, content);
+
+    //     Board newBoard = new Board();
+    //     newBoard.setWriter(writer);
+    //     newBoard.setTitle(title);
+    //     newBoard.setContent(content);
+
+    //     return ResponseEntity.ok(newBoard);
+    // }
+
     @PostMapping("/write")
-    public ResponseEntity<Board> write(@RequestParam("writer") String writer,
-            @RequestParam("title") String title,
-            @RequestParam("content") String content) {
-        boardService.write(writer, title, content);
+    public ResponseEntity<Board> write(@RequestBody Board board){
 
-        Board newBoard = new Board();
-        newBoard.setWriter(writer);
-        newBoard.setTitle(title);
-        newBoard.setContent(content);
+        boardService.write(board.getWriter(), board.getTitle(), board.getContent());
 
-        return ResponseEntity.ok(newBoard);
+        return ResponseEntity.ok(board);
+   
     }
+            
 
     // @DeleteMapping("/delete")
     // public ResponseEntity<Void> delete(@RequestParam("id") int id) {
@@ -70,4 +82,14 @@ public class MyController {
 
         return ResponseEntity.noContent().build();
     }
-}
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Board> update(@PathVariable int id, @RequestBody Board updatedBoard) 
+     { try {
+        Board board = boardService.update(id,updatedBoard.getWriter(), updatedBoard.getTitle(), updatedBoard.getContent());
+        return ResponseEntity.ok(board);
+
+     } catch (RuntimeException e) {
+        return ResponseEntity.notFound().build();
+    }
+     }}
